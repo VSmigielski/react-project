61012bd3d8420da450ab9bba1cb284fbcb1eae76
Veronica Smigielski <veronicasmigielski@gmail.com>
2021-02-01 04:03:04 +0100

    Update Search.js without ids

--- src/Search.js
+++ src/Search.js
@@ -5,18 +5,16 @@ export default function Search() {
   return (
     <div>
       <input
-        id="search-text-input"
         type="search"
         placeholder="Enter a city"
         autofocus="on"
         auto-complete="off"
       />
-      <button type="submit" id="search" className="btn btn-success">
+      <button type="submit" className="btn btn-success">
         Search
       </button>
       <button
         type="submit"
-        id="current-location-button"
         className="btn btn-primary"
       >
         Current
